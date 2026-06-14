07f9fc9dfce64722d2c811ed3b2ea589b6368533
5p2O5pav <aa316191@outlook.com>
2026-06-14 08:52:28 +0800

    Update merge.go

--- internal/db/merge.go
+++ internal/db/merge.go
@@ -6,7 +6,7 @@ import (
 	"github.com/dgraph-io/badger/v4"
 )
 
-// getUint64 从字节切片中解码 uint64（大端序）
+// GetUint64 从字节切片中解码 uint64（大端序）
 func GetUint64(val []byte) uint64 {
 	if len(val) == 0 {
 		return 0
@@ -14,7 +14,7 @@ func GetUint64(val []byte) uint64 {
 	return binary.BigEndian.Uint64(val)
 }
 
-// putUint64 将 uint64 编码为字节切片（大端序）
+// PutUint64 将 uint64 编码为字节切片（大端序）
 func PutUint64(v uint64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, v)
@@ -32,7 +32,7 @@ func GetCounter(txn *badger.Txn, ip string) (uint64, error) {
 	}
 	var val uint64
 	err = item.Value(func(v []byte) error {
-		val = getUint64(v)
+		val = GetUint64(v) // 修正：使用大写导出函数
 		return nil
 	})
 	return val, err
@@ -49,7 +49,7 @@ func GetLastSeen(txn *badger.Txn, ip string) (int64, error) {
 	}
 	var ts uint64
 	err = item.Value(func(v []byte) error {
-		ts = getUint64(v)
+		ts = GetUint64(v) // 修正：使用大写导出函数
 		return nil
 	})
 	return int64(ts), err
